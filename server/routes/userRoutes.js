d1281476d183dac4b28efb8c9673ce04950534f3
Laslaa <mohamed.laslaa@gmail.com>
2020-02-11 12:52:22 +0100

    userbyid

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 const { User } = require("../models/userSchema");
 
@@ -8,13 +7,18 @@ router.get('/', async (req, res) => {
     res.send(allUser)
 })
 
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select("firstName lastName email dateBirth");
+    res.send(user)
+})
+
 router.post('/', async (req, res) => {
     const user = new User({
         lastName: req.body.lastName,
         firstName: req.body.firstName,
         email: req.body.email,
         password: req.body.password
-    }) 
+    })
 
     await user.save();
     res.send(user)
