634fcd97c8914f7a260dfe8f1d678aa91303e026
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-07-04 15:20:14 +0200

    heroku desploy test8

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
+  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
